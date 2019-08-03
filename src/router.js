0d8e615b3cad68f7bbbd11c2d93011a91507ae5d
shomitarai <shomitarai1@gmail.com>
2019-08-03 18:59:38 +0900

    #4 ADD Log in function

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from "vue-router";
 import Index from "@/views/Index";
 import AddSmoothie from "@/views/AddSmoothie";
 import EditSmoothie from "@/views/EditSmoothie";
+import Login from "@/views/auth/Login";
+import Signup from "@/views/auth/Signup";
 
 Vue.use(Router);
 
@@ -24,6 +26,16 @@ export default new Router({
       path: "/edit-smoothie",
       name: "EditSmoothie",
       component: EditSmoothie
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login
+    },
+    {
+      path: "/signup",
+      name: "Signup",
+      component: Signup
     }
   ]
 });
---
+++ src/views/auth/Signup.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="signup container">
+    <form @submit.prevent="signup" class="card-panel">
+      <h2 class="indigo-text">Signup</h2>
+
+      <div class="field">
+        <label for="userId">User ID:</label>
+        <input type="text" name="userId" v-model="userId" />
+      </div>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="text" name="email" v-model="email" />
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password" />
+      </div>
+      <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn indigo">Signup</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+import firebase from 'firebase'
+import functions from 'firebase/functions'
+
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      email: null,
+      userId: null,
+      password: null,
+      feedback: null
+    }
+  },
+  methods: {
+    signup() {
+      if (this.email && this.userId && this.password) {
+        this.slug = slugify(this.userId, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+        // TODO check user ID process
+        let checkUserId = firebase.functions().httpsCallable('checkUserId')
+        checkUserId({ slug: this.slug })
+          .then(result => {
+            if (!result.data.unique) {
+              this.feedback = 'This User ID already exists'
+            } else {
+              firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(cred => {
+                  db.collection('users')
+                    .doc(this.slug)
+                    .set({
+                      user_id: this.userId,
+                      uid: cred.user.uid
+                    })
+                })
+                .then(() => {
+                  this.$router.push({ name: 'Index' })
+                })
+                .catch(err => {
+                  this.feedback = err.message
+                })
+            }
+          })
+          .catch(err => {
+            this.feedback = err.message
+          })
+      } else {
+        this.feedback = 'You must enter a User ID, Email and Password.'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.signup {
+  max-width: 400px;
+  margin-top: 60px;
+}
+.signup h2 {
+  font-size: 2.4em;
+  margin-top: 20px;
+  margin-bottom: 30px;
+}
+
+.signup .field {
+  margin-bottom: 16px;
+}
+</style>

d31128cbf352b071e5254ef8752276634dc75f0c
Sho Mitarai <shomitarai1@gmail.com>
2019-08-31 19:22:12 +0900

    #37 Fix firebase auth control

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let baseApp
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!baseApp) {
+    baseApp = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
